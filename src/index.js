93b80b9da26d718215e43bfa797cb5e6b770ba72
Keegan Street <keegan.street@rea-group.com>
2021-03-02 16:29:01 +1100

    Example of broken externals in Webpack 5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import http from "http";
+
 export default () => {
-  console.log("I am a library");
+  console.log(http);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   mode: "development",
-  output: {
-    library: "A",
-  },
-  optimization: {
-    runtimeChunk: "single"
-  },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ],
+  target: "node",
+  externals: {
+    http: "external_http"
+  }
 };
